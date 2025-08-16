a9c3a8f5986df21f401001fe292db90ff74645e8
RohanPrakash305 <atulraj844114@gmail.com>
2025-08-16 23:05:48 +0530

    second updet

--- ndex.js
+++ ndex.js
@@ -11,14 +11,17 @@
 // const arr=[1,2,3,4,5,6]
 // console.log(arr[0])
 
-const app=require ('./app')
+// const app=require ('./app')
 // console.log(app.z());
 
 
-const arr=[1,2,3,4,5,6,3,6,3];
+// const arr=[1,2,3,4,5,6,3,6,3];
 
-let result = arr.filter((item)=>{
-    return item ===3
+// let result = arr.filter((item)=>{
+//     return item ===3
 
-})
-console.log(result)
+// })
+// console.log(result)
+const fs=require('fs');
+
+fs.writeFileSync("xxxt.txt","hello rohan")
