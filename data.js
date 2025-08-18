6a59ff578e87062d5be06b355f476f6a263b5bd1
RohanPrakash305 <atulraj844114@gmail.com>
2025-08-19 01:12:12 +0530

    nodemon install and use postMan

---
+++ data.js
@@ -0,0 +1,12 @@
+
+const data = [
+    {name:'Anil si',email:'anil@test.com'},
+
+    {name:'rohan si',email:'rohan@test.com'},
+
+    {name:'Atul si',email:'atul@test.com'}
+
+
+]
+
+module.exports = data ;
--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 
 // gs("acd.txt","hiiii")
 
-const abc = require('http');
+
+
+
+
+
+// const abc = require('http');
 
 
 // abc.createServer((req,res)=>{
@@ -21,7 +26,24 @@ const abc = require('http');
 
 
 
-abc.createServer((req, res) => {
-    res.write("hello rohan jee");
-    res.end();
-}).listen(4500);
+// abc.createServer((req, res) => {
+//     res.write("hello rohan jee");
+//     res.end();
+// }).listen(4500);
+
+// var colors = require('colors');
+// console.log("hello".blue)
+// console.log("hello my frend".red)
+
+
+
+const http = require('http');
+const data = require('./data')
+
+http.createServer((req,res)=>{
+
+    res.writeHead(404,{'content-type':'application\json'});
+
+    res.write(JSON.stringify(data));
+   res.end();
+}).listen(5000);
