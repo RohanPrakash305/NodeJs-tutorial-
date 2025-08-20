9a1355ad29f2ee589c820d02a1a62b4f999fd698
RohanPrakash305 <atulraj844114@gmail.com>
2025-08-20 12:57:07 +0530

    Read and Write file by commmand

--- index.js
+++ index.js
@@ -52,20 +52,38 @@
 // console.log(process.argv[2])
 // console.log(process.argv[3])
 
+// ........................................
+// const fs = require('fs');
+// const input = process.argv;
+
+// if(input[2]=='add')
+// {
+//     fs.writeFileSync(input[3],input[4])
+// }
+// else if(input[2]=='remove')
+// {
+//     fs.unlinkSync(input[3])
+
+// }
+// else{
+//     console.log("invalid input")
+// }
+
 const fs = require('fs');
 
+const path = require('path');
+const dirPath = path.join(__dirname,'files');
 
-const input = process.argv;
-
-if(input[2]=='add')
+for(i=0;i<5;i++)
 {
-    fs.writeFileSync(input[3],input[4])
+    fs.writeFileSync(dirPath+"/hello"+i+".txt","a simple tet file");
 }
-else if(input[2]=='remove')
-{
-    fs.unlinkSync(input[3])
 
-}
-else{
-    console.log("invalid input")
-}
+fs.readdir(dirPath,(err,file)=>{
+
+    console.warn(file)
+
+    file.forEach((item)=>{
+        console.warn(item);
+    })
+})
